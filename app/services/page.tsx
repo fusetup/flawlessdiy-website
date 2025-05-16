b7f00ba726bc45dca818c4d4f514477e9ce61e0b
aksanood <aksanood@gmail.com>
2025-05-17 00:51:17 +0100

    Added SEO metadata

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import Image from "next/image"
 import { services } from "@/lib/services"
 import {
-  Tree,
+  Trees,
   Shovel,
   Clock,
   Award,
@@ -40,10 +40,44 @@ export const metadata: Metadata = {
     images: ["/flawlessdiy_logo.png"],
     site: "@flawlessdiy",
   },
+  keywords: [
+    "handyman Warwickshire",
+    "DIY services Leamington Spa",
+    "home improvement Warwick",
+    "property maintenance Kenilworth",
+    "gardening Stratford-upon-Avon",
+    "gardening Leamington Spa",
+    "local handyman near me",
+    "Flawless DIY Warwickshire",
+    "affordable handyman Warwickshire",
+    "Warwickshire home repairs",
+    "kitchen fitting Warwickshire",
+    "painting and decorating Leamington Spa",
+    "cleaning services Warwickshire",
+    "handyman Coventry",
+    "handyman Rugby",
+    "handyman Nuneaton",
+    "handyman Bedworth",
+    "handyman Atherstone",
+    "handyman Southam",
+    "handyman Alcester",
+    "handyman Shipston-on-Stour",
+    "handyman Coleshill",
+    "handyman Polesworth",
+    "handyman Whitnash",
+    "handyman Wellesbourne",
+    "handyman Studley",
+    "handyman Henley-in-Arden",
+    "handyman Bulkington",
+    "handyman Kineton",
+    "handyman Dunchurch",
+    "handyman Bishops Tachbrook",
+    "flawlessdiy"
+  ],
 };
 
 const iconMap = {
-  Tree,
+  Trees,
   Shovel,
   Clock,
   Award,
@@ -74,7 +108,7 @@ export default function ServicesPage() {
             {services.map((service, index) => (
               <div
                 key={service.id}
-                id={service.id}
+                id={service.id.toString()}
                 className={`flex flex-col ${index % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"} gap-8 items-center scroll-mt-24`}
               >
                 <div className="md:w-1/2 relative h-[300px] w-full rounded-lg overflow-hidden">
@@ -88,7 +122,7 @@ export default function ServicesPage() {
                 <div className="md:w-1/2">
                   <div className="mb-4">
                     {(() => {
-                      const Icon = iconMap[service.icon];
+                      const Icon = iconMap[service.icon as keyof typeof iconMap];
                       return Icon && <Icon className="h-10 w-10 text-green-500" />;
                     })()}
                   </div>
@@ -113,6 +147,37 @@ export default function ServicesPage() {
           </Button>
         </div>
       </section>
+
+      {/* LocalBusiness Structured Data for SEO */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'LocalBusiness',
+            name: 'Flawless DIY',
+            image: 'https://flawlessdiy.co.uk/flawlessdiy_logo.png',
+            '@id': 'https://flawlessdiy.co.uk',
+            url: 'https://flawlessdiy.co.uk',
+            telephone: '+44 7513 912686',
+            email: 'victor@flawlessdiy.co.uk',
+            address: {
+              '@type': 'PostalAddress',
+              streetAddress: '46 Monarch Gardens',
+              addressLocality: 'Leamington Spa',
+              addressRegion: 'Warwickshire',
+              postalCode: 'CV31 3LX',
+              addressCountry: 'GB',
+            },
+            areaServed: 'Warwickshire',
+            description: 'Professional DIY, handyman, and home improvement services in Leamington Spa and Warwickshire.',
+            sameAs: [
+              'https://www.facebook.com/flawlessdiy',
+              'https://www.instagram.com/flawlessdiy',
+            ],
+          }),
+        }}
+      />
     </div>
   )
 }
